edeab0484adec3c285ef960048a6541f5cd59451
Hung Tran <hung.tran@swissitbridge.com>
2017-12-12 08:27:56 +0700

    finished css coding tips. finished semi-transparent border

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
+import { Routes, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CodingTipsComponent } from './coding-tips/coding-tips.component';
+import { BordersComponent } from './borders/borders.component';
 
+const ROUTES: Routes = [
+    {
+        path: 'coding-tips',
+        component: CodingTipsComponent
+    },
+    {
+        path: 'borders',
+        component: BordersComponent
+    }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CodingTipsComponent,
+    BordersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/coding-tips/coding-tips.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-coding-tips',
+  templateUrl: './coding-tips.component.html',
+  styleUrls: ['./coding-tips.component.scss']
+})
+export class CodingTipsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
